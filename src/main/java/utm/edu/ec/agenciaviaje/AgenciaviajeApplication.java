6f7168a3ee233d482bc92b2832f5c2b994a334a5
josselynstefania <33069261+josselynstefania@users.noreply.github.com>
2022-04-29 10:52:38 -0500

    initial commit

---
+++ src/main/java/utm/edu/ec/agenciaviaje/AgenciaviajeApplication.java
@@ -0,0 +1,13 @@
+package utm.edu.ec.agenciaviaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaviajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaviajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utm/edu/ec/agenciaviaje/AgenciaviajeApplicationTests.java
@@ -0,0 +1,13 @@
+package utm.edu.ec.agenciaviaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaviajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
